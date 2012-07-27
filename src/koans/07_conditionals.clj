d07407c8bcb6078548b665097a739a22219f5861
Eric MacAdie <Eric@MacAdie.net>
2012-07-27 15:46:28 -0500

    got through 07

--- src/koans/07_conditionals.clj
+++ src/koans/07_conditionals.clj
@@ -9,38 +9,63 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))
           :a
           :b))
+  ;  (= __ (if (false? (= 4 5))
+  ;        :a
+  ;        :b))
 
+
+  
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
           []))
+  ;(= __ (if (> 4 3)
+  ;        []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
+  (= nil (if (nil? 0)
           [:a :b :c]))
+  ;(= __ (if (nil? 0)
+  ;        [:a :b :c]))
 
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
               :doom
-              __))
+              :glory))
+  ;(= :glory (if (not (empty? ()))
+  ;            :doom
+  ;            __))
 
   "You may have a multitude of possible paths"
+  ;(let [x 5]
+  ;  (= :your-road (cond (= x __) :road-not-taken
+  ;                      (= x __) :another-road-not-taken
+  ;                      :else __)))
+
   (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
+    (= :your-road (cond (= x 4) :road-not-taken
+                        (= x 3) :another-road-not-taken
+                        :else   :your-road)))
 
   "Or your fate may be sealed"
-  (= __ (if-not (zero? __)
+  ;(= __ (if-not (zero? __)
+  ;        'doom
+  ;        'doom))
+  ; look this one up
+  (= 'doom (if-not (zero? 0)
           'doom
           'doom))
 
   "In case of emergency, sound the alarms"
+  ; (= :sirens
+  ;   (explain-defcon-level __))
   (= :sirens
-     (explain-defcon-level __))
+     (explain-defcon-level :cocked-pistol))
 
   "But admit it when you don't know what to do"
-  (= __
+  ;(= __
+   ;  (explain-defcon-level :yo-mama)))
+  (= :say-what?
      (explain-defcon-level :yo-mama)))
